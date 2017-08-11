89b97aba853dc795602b96f843b8b0c70c37db37
gauravATincedo <gaurav.verma@incedoinc.com>
2017-08-11 22:51:32 +0530

    as
    sa

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -8,546 +8,64 @@ var shoovWebdrivercss = require('shoov-webdrivercss');
 // PROVIDER_PREFIX=browserstack SELECTED_CAPS=iphone5 mocha
 //gaurav verma
 
-var capsConfig = { 'chrome': {
+var capsConfig = { 
+'chrome_win10': {
     'browser' : 'Chrome',
     'browser_version' : '60.0',
     'os' : 'Windows',
     'os_version' : '10',
     'resolution' : '1366x768'
   },
-'chrome2': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1024x768'
-  },
-'chrome3': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1280x800'
-  },
-'chrome4': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1280x1024'
-  },
-'chrome5': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1440x900'
-  },
-'chrome6': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1680x1050'
-  },
-'chrome7': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1600x1200'
-  },
-'chrome8': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1920x1200'
-  },
-'chrome9': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1920x1080'
-  },
-'chrome10': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '2048x1536'
-  },
-'IE1': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1024x768'
-  },
-'IE2': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1280x800'
-  },
-'IE3': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1280x1024'
-  },
-'IE4': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1440x900'
-  },
-'IE5': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1680x1050'
-  },
-'IE6': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1600x1200'
-  },
-'IE7': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1920x1200'
-  },
-'IE8': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1920x1080'
-  },
-'IE9': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '2048x1536'
-  },
-'IE10': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1366x768'
-  },
-'Firefox1': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1024x768'
-  },
-'Firefox2': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1280x800'
-  },
-'Firefox3': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1280x1024'
-  },
-'Firefox4': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1440x900'
-  },
-'Firefox5': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1680x1050'
-  },
-'Firefox6': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1600x1200'
-  },
-'Firefox7': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1920x1200'
-  },
-'Firefox8': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1920x1080'
-  },
-'Firefox9': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '2048x1536'
-  },
-'Firefox10': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '10',
-    'resolution' : '1366x768'
-  },
-'IE22': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '800x600'
-  },
-'IE23': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1024x768'
-  },
-'IE24': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1280x800'
-  },
-'IE25': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1280x1024'
-  },
-'IE26': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1366x768'
-  },
-'IE27': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1440x900'
-  },
-'IE28': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1680x1050'
-  },
-'IE29': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1600x1200'
-  },
-'IE30': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1920x1200'
-  },
-'IE31': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1920x1080'
-  },
-'IE32': {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '2048x1536'
-  },
-'chrome22': {
+'chrome_win7': {
     'browser' : 'Chrome',
     'browser_version' : '60.0',
     'os' : 'Windows',
     'os_version' : '7',
     'resolution' : '800x600'
   },
-'chrome23': {
+'chrome_mac_Sierra': {
     'browser' : 'Chrome',
     'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '7',
+    'os' : 'Mac',
+    'os_version' : 'Sierra',
     'resolution' : '1024x768'
   },
-'chrome24': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
+  'IE11_win10': {
+    'browser' : 'IE',
+    'browser_version' : '11.0',
+    'os' : 'Windows',
+    'os_version' : '10',
+    'resolution' : '1024x768'
+  },
+'IE11_win7': {
+    'browser' : 'IE',
+    'browser_version' : '11.0',
     'os' : 'Windows',
     'os_version' : '7',
+    'resolution' : '800x600'
+  },
+'Firefox_win10': {
+    'browser' : 'Firefox',
+    'browser_version' : '54.0',
+    'os' : 'Windows',
+    'os_version' : '10',
     'resolution' : '1280x800'
   },
-'chrome25': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1280x1024'
-  },
-'chrome26': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1366x768'
-  },
-'chrome27': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1440x900'
-  },
-'chrome28': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1680x1050'
-  },
-'chrome29': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1600x1200'
-  },
-'chrome30': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1920x1200'
-  },
-'chrome31': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1920x1080'
-  },
-'chrome32': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '2048x1536'
-  },
-'Firefox33': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '800x540'
-  },
-'Firefox34': {
+'Firefox_win7': {
     'browser' : 'Firefox',
     'browser_version' : '54.0',
     'os' : 'Windows',
     'os_version' : '7',
     'resolution' : '1024x768'
   },
-'Firefox35': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1280x800'
-  },
-'Firefox36': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1280x1024'
-  },
-'Firefox37': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1366x768'
-  },
-'Firefox38': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1440x900'
-  },
-'Firefox39': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1680x1050'
-  },
-'Firefox41': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1540x1200'
-  },
-'Firefox42': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1920x1200'
-  },
-'Firefox43': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1920x1080'
-  },
-'Firefox44': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '2048x1536'
-  },
-'chrome44': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1024x768'
-  },
-'chrome45': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1280x960'
-  },
-'chrome46': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1280x1024'
-  },
-'chrome47': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1600x1200'
-  },
-'chrome48': {
-    'browser' : 'Chrome',
-    'browser_version' : '60.0',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1920x1080'
-  },
-'Firefox77': {
+'Firefox_Mac_Sierra': {
     'browser' : 'Firefox',
     'browser_version' : '54.0',
     'os' : 'Mac',
     'os_version' : 'Sierra',
     'resolution' : '1024x768'
   },
-'Firefox78': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1280x960'
-  },
-'Firefox79': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1280x1024'
-  },
-'Firefox80': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1600x1200'
-  },
-'Firefox81': {
-    'browser' : 'Firefox',
-    'browser_version' : '54.0',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1920x1080'
-  },
-'Safari77': {
-    'browser' : 'Safari',
-    'browser_version' : '10.1',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1024x768'
-  },
-'Safari78': {
-    'browser' : 'Safari',
-    'browser_version' : '10.1',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1280x960'
-  },
-'Safari79': {
-    'browser' : 'Safari',
-    'browser_version' : '10.1',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1280x1024'
-  },
-'Safari80': {
-    'browser' : 'Safari',
-    'browser_version' : '10.1',
-    'os' : 'Mac',
-    'os_version' : 'Sierra',
-    'resolution' : '1600x1200'
-  },
-'Safari81': {
+'Safari_Mac_Sierra': {
     'browser' : 'Safari',
     'browser_version' : '10.1',
     'os' : 'Mac',
@@ -580,20 +98,19 @@ describe('Visual monitor testing', function() {
     shoovWebdrivercss.after(done);
   });
 
-  it('should show the home page',function(done) {
-    client
-      .url(baseUrl)
-      .webdrivercss(testName + '.chrome', {
-        name: '1',
-        exclude: [],
-        remove: [],
-        hide: [],
-        screenWidth: selectedCaps == 'chrome' 
-      }, resultsCallback)
-      .call(done);
-  });
+  console.log(testName + '_' + caps.os + '_' + caps.os_version + '_' + caps.resolution);
 
+   it('should show the home page',function(done) {
+   client
+     .url(baseUrl)
+     .webdrivercss(testName + '_' + caps.os + '_' + caps.os_version, {
+       name: '1',
+       exclude: [],
+       remove: [],
+       hide: [],
+       screenWidth: selectedCaps == '__' ? [350,640, 960, 1200,1366,800,1024,1280,1440,1600,1680,1920] : undefined,
+     }, resultsCallback)
+     .call(done);
+ });
 
-
-  
 });
